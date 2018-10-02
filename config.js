1c2a7a3a8c1434e3972851c654a6f319ebe9ba92
Kaushal Patankar <kvp2804@yahoo.com>
2018-10-02 11:44:56 +0530

    updated database

--- config.js
+++ config.js
@@ -1,6 +1,9 @@
 module.exports = {
 
-	"database": "mongodb://localhost:27017/mediapp",
+	/*
+	Use this link for local database
+	"database": "mongodb://localhost:27017/mediapp",*/
+	"database": "mongodb://kp:kp1234@ds221003.mlab.com:21003/mediapp",
 	"port": process.env.PORT || 3000,
 	"secretKey": "YourSecretKey"
 }
